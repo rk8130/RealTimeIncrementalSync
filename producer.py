7a79d955070be7dd38b86555e551792bf3c7266e
rk8130 <rishukumar2001@gmail.com>
2024-12-05 23:48:34 +0530

    updated_producer_code

--- producer.py
+++ producer.py
@@ -3,7 +3,7 @@ import threading
 from decimal import *
 from time import sleep
 from uuid import uuid4, UUID
-import time
+import json
 
 from confluent_kafka import SerializingProducer
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -11,72 +11,6 @@ from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.serialization import StringSerializer
 import pandas as pd
 import mysql.connector
-
-
-# Define Kafka configuration
-kafka_config = {
-    'bootstrap.servers': 'pkc-921jm.us-east-2.aws.confluent.cloud:9092',
-    'sasl.mechanisms': 'PLAIN',
-    'security.protocol': 'SASL_SSL',
-    'sasl.username': 'YXVAJ4PHZNL5SVMA',
-    'sasl.password': 'NWyUC/CXlmeVKB5N+ZAJb7erBDyRoLMftsXCWcs1lbH8x10l81d6F37mrS8rJNCD'
-}
-
-# Schema for User record value
-schema_registry_client=SchemaRegistryClient({
-    'url': 'https://psrc-lo3do.us-east-2.aws.confluent.cloud',
-    'basic.auth.user.info': '{}:{}'.format('U2VGBECL4ZMIH54Pl', 'QAAP/kLS1XXOMRJ2xkViG6gslGaLVdFrR9ET5JCb2XKk2GQrHmBM6h5ASRTDRCOJ')
-})
-
-# Fetch the schema by subject name
-subject_name = 'product_updates-value'
-schema_str=schema_registry_client.get_latest_version(subject_name).schema.schema_str
-
-
-# Avro serializer for User record value
-key_serializer = StringSerializer('utf_8')
-avro_serializer = AvroSerializer(schema_str, schema_registry_client)
-
-# Define the serializiing producer
-producer=SerializingProducer({
-    'bootstrap.servers': kafka_config['bootstrap.servers'],
-    'key.serializer': key_serializer,
-    'value.serializer': avro_serializer,
-    'sasl.mechanisms': kafka_config['sasl.mechanisms'],
-    'security.protocol': kafka_config['security.protocol'],
-    'sasl.username': kafka_config['sasl.username'],
-    'sasl.password': kafka_config['sasl.password']
-})
-
-
-
-
-
-
-# Define MySQL configuration
-mysql_config={
-    'host':'localhost',
-    'user':'root',
-    'password':'12345',
-    'database':'project2'
-}
-
-mydb = mysql.connector.connect(
-    host=mysql_config['host'], 
-    user=mysql_config['user'], 
-    passwd=mysql_config['password'], 
-    database=mysql_config['database']
-)
-
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM product")
-rows=mycursor.fetchall()
-# Extracting the first column (id) and the rest of the columns (value)
-id = [row[0] for row in rows]  # First column
-value = [row[1:] for row in rows]  # All columns except the first one
-mycursor.close()
-mydb.close()
-
 
 
 
@@ -105,3 +39,120 @@ def delivery_report(err, msg):
         return
     print('User record {} successfully produced to {} [{}] at offset {}'.format(
         msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+
+
+
+# Define Kafka configuration
+kafka_config = {
+    'bootstrap.servers': 'pkc-921jm.us-east-2.aws.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'YXVAJ4PHZNL5SVMA',
+    'sasl.password': 'NWyUC/CXlmeVKB5N+ZAJb7erBDyRoLMftsXCWcs1lbH8x10l81d6F37mrS8rJNCD'
+}
+
+# Schema for User record value
+schema_registry_client=SchemaRegistryClient({
+    'url': 'https://psrc-lo3do.us-east-2.aws.confluent.cloud',
+    'basic.auth.user.info': '{}:{}'.format('U2VGBECL4ZMIH54P', 'QAAP/kLS1XXOMRJ2xkViG6gslGaLVdFrR9ET5JCb2XKk2GQrHmBM6h5ASRTDRCOJ')
+})
+
+# Fetch the schema by subject name
+subject_name = 'product_updates-value'
+schema_str=schema_registry_client.get_latest_version(subject_name).schema.schema_str
+
+
+# Avro serializer for User record value
+key_serializer = StringSerializer('utf_8')
+avro_serializer = AvroSerializer(schema_registry_client, schema_str)
+
+
+
+# Define the serializiing producer
+producer=SerializingProducer({
+    'bootstrap.servers': kafka_config['bootstrap.servers'],
+    'key.serializer': key_serializer,
+    'value.serializer': avro_serializer,
+    'sasl.mechanisms': kafka_config['sasl.mechanisms'],
+    'security.protocol': kafka_config['security.protocol'],
+    'sasl.username': kafka_config['sasl.username'],
+    'sasl.password': kafka_config['sasl.password']
+})
+
+
+# Define MySQL configuration
+mysql_config={
+    'host':'localhost',
+    'user':'root',
+    'password':'12345',
+    'database':'project2'
+}
+
+
+# Connect to MySQL database
+mydb = mysql.connector.connect(
+    host=mysql_config['host'], 
+    user=mysql_config['user'], 
+    passwd=mysql_config['password'], 
+    database=mysql_config['database']
+)
+
+mycursor = mydb.cursor()
+
+
+# Load the last read timestamp from the config file
+config_data = {}
+
+try:
+    with open('config.json') as f:
+        config_data = json.load(f)
+        last_read_timestamp = config_data.get('last_read_timestamp')
+except FileNotFoundError:
+    pass
+
+# Set a default value for last_read_timestamp
+if last_read_timestamp is None:
+    last_read_timestamp = '1900-01-01 00:00:00'
+
+
+query = "SELECT * FROM product WHERE last_updated > '{}'".format(last_read_timestamp)
+
+# Fetch the data from MySQL database
+mycursor.execute(query)
+
+rows=mycursor.fetchall()
+
+
+for row in rows:
+    key = str(row[0])
+    value = row[:]
+    print(key,":",value)
+    # producer.produce(topic='product_updates', key=key, value=value, on_delivery=delivery_report)
+    # producer.poll(0)
+    # producer.flush()
+
+# Fetch any remaining rows to consume the result
+mycursor.fetchall()
+
+query = "SELECT MAX(last_updated) FROM product"
+mycursor.execute(query)
+
+# Fetch the result
+result = mycursor.fetchone()
+max_date = result[0]  # Assuming the result is a single value
+
+# Convert datetime object to string representation
+max_date_str = max_date.strftime("%Y-%m-%d %H:%M:%S")
+
+# Update the value in the config.json file
+config_data['last_read_timestamp'] = max_date_str
+
+with open('config.json', 'w') as file:
+    json.dump(config_data, file)
+
+# Close the cursor and database connection
+mycursor.close()
+mydb.close()
+
+print('All messages are delivered')  
